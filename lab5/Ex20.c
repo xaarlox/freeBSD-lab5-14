38d5b5096f9c99680397f8a47d80207de92505f6
Valeriia Fedorenko <xaarlox@gmail.com>
2025-04-22 17:38:19 +0300

    added Ex20.c

--- lab5/Ex20.c
+++ lab5/Ex20.c
@@ -2,51 +2,45 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <string.h>
 #include <sys/wait.h>
 
 #define PAGE_SIZE 4096
 
-void* allocate_shared_memory(size_t size) {
-    void* ptr = mmap(NULL, size, PROT_READ | PROT_WRITE,
+void* allocate_memory(size_t size) {
+    void* mem = mmap(NULL, size, PROT_READ | PROT_WRITE,
                      MAP_PRIVATE | MAP_ANON, -1, 0);
-    if (ptr == MAP_FAILED) {
+    if (mem == MAP_FAILED) {
         perror("mmap failed");
         exit(EXIT_FAILURE);
     }
-    return ptr;
+    return mem;
 }
 
-void deallocate_shared_memory(void* ptr, size_t size) {
-    if (munmap(ptr, size) == -1) {
-        perror("munmap failed");
-        exit(EXIT_FAILURE);
-    }
+void write_message(void* mem) {
+    sprintf((char*)mem, "Temporary message\n");
 }
 
-void run_child_process(void* mem) {
-    printf("Child says: %s", (char*)mem);
+void run_child() {
     _exit(0);
 }
 
-void run_parent_process(pid_t child_pid, void* mem) {
-    waitpid(child_pid, NULL, 0);
-    printf("Parent says: %s", (char*)mem);
-    deallocate_shared_memory(mem, PAGE_SIZE);
+void run_parent() {
+    wait(NULL);
+    exit(0);
 }
 
-int main(void) {
-    void* mem = allocate_shared_memory(PAGE_SIZE);
-    strcpy((char*)mem, "Hello from mmap!\n");
+int main() {
+    void* mem = allocate_memory(PAGE_SIZE);
+    write_message(mem);
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork failed");
-        deallocate_shared_memory(mem, PAGE_SIZE);
+        munmap(mem, PAGE_SIZE);
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        run_child_process(mem);
+        run_child();
     } else {
-        run_parent_process(pid, mem);
+        run_parent();
     }
     return 0;
 }
