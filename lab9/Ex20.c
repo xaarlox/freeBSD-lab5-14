c10b2e7eb1cf62755ac3fc9b166d9ed8a90d8e39
Valeriia Fedorenko <xaarlox@gmail.com>
2025-04-30 21:06:17 +0300

    added Ex20.c (lab9)

---
+++ lab9/Ex20.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
+int main() {
+    printf("Before:\n");
+    printf("UID = %d\n", getuid());
+    printf("USER = %s\n", getenv("USER"));
+    printf("HOME = %s\n", getenv("HOME"));
+    
+	setenv("USER", "bob", 1);
+    setenv("HOME", "/home/bob", 1);
+    setenv("SHELL", "/bin/sh", 1);
+
+    printf("\nAfter:\n");
+    printf("UID = %d\n", getuid());
+    printf("USER = %s\n", getenv("USER"));
+    printf("HOME = %s\n", getenv("HOME"));
+
+    execlp("sh", "sh", "-c", "echo Hello from $USER, home $HOME", (char *)NULL);
+
+    perror("execlp");
+    return 1;
+}
